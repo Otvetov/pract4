d651337f638c5e4eca7e1a869b85bc84eb86b00b
Roman Slushnyi <r.slushniy@yandex.ru>
2024-12-04 12:46:01 +0500

    feat:add signup

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,12 +4,12 @@
 
     <nav class="navbar navbar-expand-md  navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">KinoTower</a>
+        <NuxtLink class="navbar-brand" to="/">KinoTower</NuxtLink>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-          <button class="btn btn-outline-success me-3" type="submit">Sign in</button>
+          <button @click="$router.push('/signup')"  class="btn btn-outline-success me-3" type="submit">SignUp</button>
           <button class="btn btn-outline-info" type="submit">Sign up</button>
         </div>
       </div>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,5 +1,33 @@
 <script setup lang="ts">
-
+ import {useGenderStore} from "~/stores/gender";
+ const errorMessages=ref()
+ const genderStore=useGenderStore()
+ const authStore=useAuthStore()
+ const router=useRouter()
+ const fio=ref('')
+ const email=ref('')
+ const password=ref('')
+ const birthday=ref('')
+ const gender=ref(0)
+ const register=async ()=>{
+   if (fio.value
+       && email.value
+       && password.value
+       && gender.value){
+     try{
+       await authStore.signup({
+         fio:fio.value,
+         email:email.value,
+         password:password.value,
+         birthday:birthday.value,
+         gender_id:gender.value,
+       })
+       await router.push('/')
+     }catch (e:any){
+      errorMessages.value=e.message
+     }
+   }
+ }
 </script>
 
 <template>
@@ -8,34 +36,40 @@
     <div class="col md-4">
       <div class="card">
         <div class="card-body">
-          <form action="">
+          <form @submit.prevent="register" action="">
             <h1 align="center">Registration</h1>
             <div>
               <label for="email" class="form-label">Email address</label>
-              <input type="email" class="form-control" id="email" placeholder="name@example.com">
+              <input v-model="email" type="email" class="form-control" id="email" placeholder="name@example.com">
             </div>
             <div>
               <label for="name" class="form-label">Name</label>
-              <input type="name" class="form-control" id="name" >
+              <input v-model="fio" type="name" class="form-control" id="name" >
             </div>
             <div>
               <label for="password" class="form-label">password</label>
-              <input type="password" class="form-control" id="password" >
+              <input v-model="password" type="password" class="form-control" id="password" >
             </div>
             <div>
               <label for="birthday" class="form-label">birthday</label>
-              <input type="birthday" class="form-control" id="birthday" >
+              <input v-model="birthday" type="date" class="form-control" id="birthday" >
             </div>
 
             <label for="gender" class="form-label">gender</label>
-            <select class="form-select" aria-label="Default select example">
-              <option value="name" selected>gender</option>
-              <option value="1">man</option>
-              <option value="2">woman</option>
+            <select v-model="gender" class="form-select" aria-label="Default select example">
+              <option value="0"  selected>gender</option>
+              <option
+                  v-for="gender in genderStore.genders"
+                  :key="gender.id"
+                  :value="gender.id">{{ gender.name }}</option>
             </select>
+            <button class="btn btn-success" type="submit">SingUp</button>
           </form>
+          <div v-if="errorMessages"  class="alert alert-danger" role="alert">
+            {{errorMessages}}
+          </div>
         </div>
-        <button class="btn btn-success" type="submit">SingUp</button>
+
       </div>
 
     </div>
