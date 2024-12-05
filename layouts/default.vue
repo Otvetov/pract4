03ff97e3aa4ca74f8c0ea57998fe9c3991d53085
Roman Slushnyi <r.slushniy@yandex.ru>
2024-12-05 08:32:51 +0500

    feat: add sign out

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,20 +2,24 @@
 
   <header>
 
-    <nav class="navbar navbar-expand-md  navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <div class="container-fluid">
         <NuxtLink class="navbar-brand" to="/">KinoTower</NuxtLink>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse justify-content-end" id="navbarCollapse">
-          <button @click="$router.push('/signup')"  class="btn btn-outline-success me-3" type="submit">SignUp</button>
-          <button class="btn btn-outline-info" type="submit">Sign up</button>
+          <template v-if="!authStore.authData">
+            <button @click="$router.push('/signin')" class="btn btn-outline-success me-3" type="submit">Sign In</button>
+            <button @click="$router.push('/signup')" class="btn btn-outline-danger" type="submit">Sign Up</button>
+          </template>
+          <template v-else>
+            <button @click="logout" class="btn btn-outline-danger" type="submit">Sign Out</button>
+          </template>
         </div>
       </div>
     </nav>
   </header>
-
 
   <main class="flex-shrink-0">
     <div class="container">
@@ -23,16 +27,23 @@
     </div>
   </main>
 
-<!--  <footer class="footer mt-auto py-3 fixed-bottom bg-body-tertiary">-->
-<!--    <div class="container">-->
-<!--      <span class="text-body-secondary">&copy;Kino-Tower-2024</span>-->
-<!--    </div>-->
-<!--  </footer>-->
+  <footer class="footer mt-auto py-2 bg-body-tertiary fixed-bottom">
+    <div class="container">
+      <span class="text-body-secondary">&copy;Kino-Tower-2024</span>
+    </div>
+  </footer>
 
 
 
 
 </template>
 <script setup lang="ts">
+import {useRouter} from "#imports";
 
+const authStore = useAuthStore();
+const logout = async () => {
+  await  authStore.signout();
+  router.push('/');
+}
+const router = useRouter();
 </script>
--- stores/auth.js
+++ stores/auth.js
@@ -1,35 +1,68 @@
-import {defineStore} from "pinia";
 import {api} from "~/api/index.js";
 
-export const useAuthStore=defineStore('auth', ()=>{
-    const authData=ref(null)
-    const authCookie=useCookie('auth')
+export const useAuthStore = defineStore('auth', () => {
+    const authData = ref(null);
+    const authCookie = useCookie('auth');
 
-    const saveAuthData =  ()=>{
-        if(authData.value){
-            authCookie.value=btoa(JSON.stringify(authData.value))
-        }
-    }
-    const readAuthData=()=>{
-        if(authCookie.value){
-            authData.value=JSON.parse(atob(authCookie.value))
-        }
-    }
-    readAuthData()
-
-    const signup=async (data)=>{
-        try{
-            const res= await api.post('/auth/signup', data)
-            authData.value=res.data
-            saveAuthData()
-        }catch (e) {
-            throw new Error(e.response.data.message)
+    const saveAuthData = () => {
+        if (authData.value){
+            authCookie.value = btoa(JSON.stringify(authData.value));
         }
     }
 
+    const readAuthData = () => {
+        if (authCookie.value) {
+            authData.value = JSON.parse(atob(authCookie.value));
+        }
+    }
+
+    readAuthData();
+
+
+    const removeAuthData = () => {
+        if (authCookie.value) {
+            authData.value = null;
+            authCookie.value = null;
+        }
+    }
+
+
+    const signup = async (data) => {
+        try {
+            const res = await api.post('/auth/signup', data);
+            authData.value = res.data;
+            saveAuthData();
+        } catch (e){
+            throw new Error(e.response.data.message);
+        }
+    }
+
+
+    const signin = async (data) => {
+        try {
+            const res = await api.post('/auth/signin', data);
+            authData.value = res.data;
+            saveAuthData();
+        }catch (e){
+            throw new Error(e.response.data.message);
+        }
+    }
+
+    const signout = async () => {
+        api.post('/auth/signout', null,{
+            headers: {
+                authorization: `Bearer ${authCookie.value.token}`,
+            }
+        });
+        removeAuthData();
+    }
 
     return{
         authData,
         signup,
+        signin,
+        signout,
+
+
     }
-})
+});
